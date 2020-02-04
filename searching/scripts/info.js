a5c79da3219eb44bf0b4e94eca6f767f576004f7
Zdravko <zdravkohristov0@gmail.com>
2020-02-04 18:25:06 +0200

    Added searching feature
    Search feature was added to search in the site's pages. One more bug left to be fixed: if someone searches for symbol that is special for regExp, it will result in error.

---
+++ searching/scripts/info.js
@@ -0,0 +1,14 @@
+const pages = [
+    {
+        title: 'Artificial inteligence',
+        href: '#',
+        content: 'Lorem UTF-8',
+        thumbnailPath: '../media/random.png'
+    }, 
+    {
+        title: 'BRATMEE',
+        href: '#',
+        content: 'PASHATA',
+        thumbnailPath: '../media/random.png'
+    }
+];
