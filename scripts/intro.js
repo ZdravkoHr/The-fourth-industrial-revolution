2bae9fcee91d157288125d560861acc3dac403bf
Zdravko <zdravkohristov0@gmail.com>
2020-02-04 11:21:32 +0200

    Added intro

---
+++ scripts/intro.js
@@ -0,0 +1,13 @@
+const hideIntro = () => {
+    document.querySelector('.intro-background').classList.add('d-none');
+}
+
+function applyAnimation() {
+    const introHeading = document.querySelector('.intro-heading');
+    introHeading.classList.remove('not-animated');
+    setTimeout(hideIntro, 2500);
+}
+
+
+
+setTimeout(applyAnimation, 1000);
