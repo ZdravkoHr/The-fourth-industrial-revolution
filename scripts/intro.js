7b9730ffb0bdc0dd23e598af82cd25ff8124d389
Zdravko <zdravkohristov0@gmail.com>
2020-02-17 19:07:58 +0200

    Changed animation + added media

--- scripts/intro.js
+++ scripts/intro.js
@@ -5,7 +5,7 @@ const hideIntro = () => {
 function applyAnimation() {
     const introHeading = document.querySelector('.intro-heading');
     introHeading.classList.remove('not-animated');
-    setTimeout(hideIntro, 2500);
+   // setTimeout(hideIntro, 2500);
 }
 
 setTimeout(applyAnimation, 1000);
