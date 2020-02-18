7b55d7d8a3c374bf023cbfc91f90fd774367f16a
Zdravko <zdravkohristov0@gmail.com>
2020-02-18 18:02:27 +0200

    Sticky navbar

--- scripts/pages-info.js
+++ scripts/pages-info.js
@@ -3,12 +3,12 @@ const pages = [
         title: 'Artificial inteligence',
         href: '#',
         content: 'Lorem UTF-8',
-        thumbnailPath: 'media/random.png'
+        thumbnailPath: 'media/home pics/random.png'
     }, 
     {
         title: 'BRATMEE',
         href: '#',
         content: 'PASHATA',
-        thumbnailPath: 'media/random.png'
+        thumbnailPath: 'media/home pics/random.png'
     }
 ];
